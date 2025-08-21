d5b3ea332a892e14feac12e5e3e2d17dadc5dd32
Ravikumar-Indana <ravikumarindana@gmail.com>
2025-08-21 13:33:33 +0530

    ...

---
+++ numpy/001_create_basic_array.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+arr_1 = np.array([1, 2, 3])
+
+print(arr_1)
+
+print(type(arr_1))
+
+arr_2d = np.array([[11,12,13],[21,22,23]])
+print(arr_2d)
+print("arr_2d")
+print(type(arr_2d)) 
+print(arr_2d.shape)
+print(arr_2d.ndim)
+print(arr_2d.size)
+print(arr_2d.dtype)
+print(arr_2d.itemsize)
+print(arr_2d.nbytes)
+print(arr_2d.flags)
+print(arr_2d.T)  # Transpose of the array
+print(arr_2d.T.shape)
